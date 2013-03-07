bc05b5455ef20217cbdd4ea96bb9d9714db997c7
Jim Edwards <edwards.jim@gmail.com>
2013-03-07 17:51:49 +0000

    fix the fix correctly

--- pio/piolib_mod.F90
+++ pio/piolib_mod.F90
@@ -1504,7 +1504,12 @@ contains
     integer, allocatable :: lstart(:), lcount(:)
 
     ndims = size(gdims)
-#ifndef _MPISERIAL
+#ifdef _MPISERIAL
+       iodesc2%elemtype=mpidatatype
+       iodesc2%filetype=mpidatatype          
+       iodesc2%n_elemtype = 0
+       iodesc2%n_words = 0
+#else
     if(sum(iodesc%count)>0) then
        allocate(lstart(ndims),lcount(ndims))
        lstart = 0
@@ -1532,13 +1537,13 @@ contains
        iodesc2%filetype=mpi_datatype_null
 #endif
     else
-#else
        iodesc2%elemtype=mpidatatype
        iodesc2%filetype=mpidatatype          
        iodesc2%n_elemtype = 0
        iodesc2%n_words = 0
-#endif
     endif
+#endif
+    
 
 
 
