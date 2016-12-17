5d9e9679b60a035755397886fcd487c4c0d1de2c
Jim Edwards <jedwards@ucar.edu>
2016-12-17 10:58:44 -0700

    update pnetcdf too

--- ctest/runcdash-nwsc-gnu.sh
+++ ctest/runcdash-nwsc-gnu.sh
@@ -13,7 +13,7 @@ module swap intel gnu/6.1.0
 module load git/2.3.0
 module load cmake/3.0.2
 module load netcdf-mpi/4.4.1
-module load pnetcdf/1.6.1
+module load pnetcdf/1.7.0
 
 export CC=mpicc
 export FC=mpif90
