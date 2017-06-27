279ab9130d26da1712d2772230a9a1548fc8b978
Ed Hartnett <edwardjameshartnett@gmail.com>
2017-06-27 07:21:33 -0400

    more code for varlist

--- src/clib/pioc_support.c
+++ src/clib/pioc_support.c
@@ -2138,8 +2138,8 @@ int openfile_int(int iosysid, int *ncidp, int *iotype, const char *filename,
         }
 
         /* Add to the list of var_desc_t structs for this file. */
-        if ((ierr = add_to_varlist(v, rec_var, &file->varlist2)))
-            return pio_err(ios, NULL, ierr, __FILE__, __LINE__);
+        /* if ((ierr = add_to_varlist(v, rec_var, &file->varlist2))) */
+        /*     return pio_err(ios, NULL, ierr, __FILE__, __LINE__); */
     }
 
     return PIO_NOERR;
