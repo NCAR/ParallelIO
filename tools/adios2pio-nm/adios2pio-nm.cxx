95e9e1eccbd5d4f48cd4e5f76b02935c0232d162
tkurc <tkurc>
2017-11-23 13:17:07 -0500

    Will print warning if #nprocs>#files and continue

--- tools/adios2pio-nm/adios2pio-nm.cxx
+++ tools/adios2pio-nm/adios2pio-nm.cxx
@@ -740,12 +740,6 @@ int ConvertVariableTimedPutVar(ADIOS_FILE **infile, std::vector<int> wfiles, int
    	                	<< " at " << __func__ << ":" << __LINE__ << endl;
    	        	}
 			}
-			/*
-            cout << "ERROR: put_vara of arrays over time is not supported yet. "
-                    << "Variable \"" << varname << "\" is a "
-                    << vi->ndim << "D array including the unlimited dimension"
-                    << endl;
-			*/
         }
     }
     adios_free_varinfo(vi);
@@ -1089,7 +1083,7 @@ void ConvertBPFile(string infilepath, string outfilename, int pio_iotype)
 		TimerStop(write);
 		TimerStart(read);
 
-		MPI_Barrier(MPI_COMM_WORLD);
+		MPI_Barrier(comm);
 		for (int i=0;i<num_infiles;i++) 
 			adios_read_close(infile[i]);
 		TimerStop(read);
@@ -1147,10 +1141,15 @@ enum PIO_IOTYPE GetIOType(string t)
 int main (int argc, char *argv[])
 {
     int ret = 0;
+
+	MPI_Comm w_comm = MPI_COMM_WORLD;
+	int w_mpirank;
+	int w_nproc;
+
     MPI_Init(&argc, &argv);
-    MPI_Comm_set_errhandler(comm, MPI_ERRORS_RETURN);
-    MPI_Comm_rank(comm, &mpirank);
-    MPI_Comm_size(comm, &nproc);
+    MPI_Comm_set_errhandler(w_comm, MPI_ERRORS_RETURN);
+    MPI_Comm_rank(w_comm, &w_mpirank);
+    MPI_Comm_size(w_comm, &w_nproc);
 
     if (argc < 4) {
         usage(argv[0]);
@@ -1159,6 +1158,29 @@ int main (int argc, char *argv[])
     
     string infilepath  = argv[1];
     string outfilename = argv[2];
+
+	/* 
+ 	 * Check if the number of nodes is less than or equal to the number of BP files.
+ 	 * If not, create a new comm.
+ 	 *
+ 	 */
+	int num_files = GetNumOfFiles(infilepath); 
+	int io_proc   = 0;
+	if (num_files<w_nproc) {
+		fprintf(stderr, "Warning: #files: %d < #procs: %d\n",num_files,w_nproc); 
+		fflush(stderr);
+		if (w_mpirank<num_files) /* I/O nodes */
+			io_proc = 1; 
+		MPI_Comm_split(w_comm,io_proc,w_mpirank,&comm);
+    	MPI_Comm_rank(comm, &mpirank);
+    	MPI_Comm_size(comm, &nproc);
+	} else {
+		io_proc = 1;
+		comm    = w_comm;
+		mpirank = w_mpirank;
+		nproc   = w_nproc;
+	}
+	
 
 #ifdef TIMING
     /* Initialize the GPTL timing library. */
@@ -1169,30 +1191,31 @@ int main (int argc, char *argv[])
     TimerInitialize();
 
     try {
-        enum PIO_IOTYPE pio_iotype = GetIOType(argv[3]);
-        InitPIO();
-        ConvertBPFile(infilepath, outfilename, pio_iotype);
-        PIOc_finalize(iosysid);
-        TimerReport(comm);
-    }
-    catch (std::invalid_argument &e)
-    {
+		if (io_proc) {
+        	enum PIO_IOTYPE pio_iotype = GetIOType(argv[3]);
+        	InitPIO();
+        	ConvertBPFile(infilepath, outfilename, pio_iotype);
+        	PIOc_finalize(iosysid);
+        	TimerReport(comm);
+		}
+		MPI_Barrier(w_comm);
+    } catch (std::invalid_argument &e) {
         std::cout << e.what() << std::endl;
         usage(argv[0]);
         return 2;
-    }
-    catch (std::exception &e)
-    {
+    } catch (std::exception &e) {
         std::cout << e.what() << std::endl;
         return 3;
     }
 
     MPI_Finalize();
     TimerFinalize();
+
 #ifdef TIMING
     /* Finalize the GPTL timing library. */
     if ((ret = GPTLfinalize ()))
         return ret;
 #endif
+
     return ret;
 }
