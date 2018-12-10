fde26f6aaa0f40e48ed6301687cfd5b20b41ab63
jayeshkrishna <jayesh@mcs.anl.gov>
2018-12-10 10:47:12 -0600

    Removing tests that result in assert fails
    check_netcdf uses asserts to ensure that ios or file passed by
    the caller is valid.
    
    Commenting out the tests that would result in these asserts (The
    older code returned error codes in this case)

--- tests/cunit/test_pioc.c
+++ tests/cunit/test_pioc.c
@@ -558,10 +558,13 @@ int check_strerror_netcdf(int my_rank)
         return ERR_WRONG;
     if (pio_err(NULL, NULL, 0, __FILE__, __LINE__))
         return ERR_WRONG;
+    /* check_netcdf now uses asserts to ensure ios or file is valid */
+    /*
     if (check_netcdf(NULL, 0, __FILE__, __LINE__))
         return ERR_WRONG;
     if (check_netcdf2(NULL, NULL, 0, __FILE__, __LINE__))
         return ERR_WRONG;
+    */
 
     /* When called with other error messages, these functions should
      * return PIO_EIO. */
