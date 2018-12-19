e67931c483c54a23b2ce14ed2b577299e766a095
dqwu <wuda@mcs.anl.gov>
2018-12-19 10:33:50 -0600

    Importing a ROMIO patch from PnetCDF's E3SM-IO benchmark program
    This patch uses a wrapper function to override ROMIO function
    ADIOI_Type_create_hindexed_x. For large counts this change
    reduces the cost of MPI_Type_contiguous
    
    This patch is provided by Wei-keng Liao (PnetCDF key developer),
    which shows noticeable performance improvement on Cori.
    
    Note, this patch has been incorporated into MPICH 3.3 release.

--- src/clib/pioc_support.c
+++ src/clib/pioc_support.c
@@ -2703,3 +2703,134 @@ const char *get_var_desc_str(int ncid, int varid, const char *desc_prefix)
 
     return file->varlist[varid].vdesc;
 }
+
+/* A ROMIO patch from PnetCDF's E3SM-IO benchmark program
+ * https://github.com/Parallel-NetCDF/E3SM-IO/blob/master/romio_patch.c */
+#if defined(MPICH_NUMVERSION) && (MPICH_NUMVERSION < 30300000)
+/*
+ *
+ *   Copyright (C) 2014 UChicgo/Argonne, LLC.
+ *   See COPYRIGHT notice in top-level directory.
+ */
+
+/* utility function for creating large contiguous types: algorithim from BigMPI
+ * https://github.com/jeffhammond/BigMPI */
+
+static int type_create_contiguous_x(MPI_Count count, MPI_Datatype oldtype, MPI_Datatype * newtype)
+{
+    /* to make 'count' fit MPI-3 type processing routines (which take integer
+     * counts), we construct a type consisting of N INT_MAX chunks followed by
+     * a remainder.  e.g for a count of 4000000000 bytes you would end up with
+     * one 2147483647-byte chunk followed immediately by a 1852516353-byte
+     * chunk */
+    MPI_Datatype chunks, remainder;
+    MPI_Aint lb, extent, disps[2];
+    int blocklens[2];
+    MPI_Datatype types[2];
+
+    /* truly stupendously large counts will overflow an integer with this math,
+     * but that is a problem for a few decades from now.  Sorry, few decades
+     * from now! */
+    assert(count / INT_MAX == (int) (count / INT_MAX));
+    int c = (int) (count / INT_MAX);    /* OK to cast until 'count' is 256 bits */
+    int r = count % INT_MAX;
+
+    MPI_Type_vector(c, INT_MAX, INT_MAX, oldtype, &chunks);
+    MPI_Type_contiguous(r, oldtype, &remainder);
+
+    MPI_Type_get_extent(oldtype, &lb, &extent);
+
+    blocklens[0] = 1;
+    blocklens[1] = 1;
+    disps[0] = 0;
+    disps[1] = c * extent * INT_MAX;
+    types[0] = chunks;
+    types[1] = remainder;
+
+    MPI_Type_create_struct(2, blocklens, disps, types, newtype);
+
+    MPI_Type_free(&chunks);
+    MPI_Type_free(&remainder);
+
+    return MPI_SUCCESS;
+}
+
+/* like MPI_Type_create_hindexed, except array_of_lengths can be a larger datatype.
+ *
+ * Hindexed provides 'count' pairs of (displacement, length), but what if
+ * length is longer than an integer?  We will create 'count' types, using
+ * contig if length is small enough, or something more complex if not */
+
+int __wrap_ADIOI_Type_create_hindexed_x(int count,
+                                 const MPI_Count array_of_blocklengths[],
+                                 const MPI_Aint array_of_displacements[],
+                                 MPI_Datatype oldtype, MPI_Datatype * newtype)
+{
+    int i, ret;
+    MPI_Datatype *types;
+    int *blocklens;
+    int is_big = 0;
+
+    types = (MPI_Datatype*) malloc(count * sizeof(MPI_Datatype));
+    blocklens = (int*) malloc(count * sizeof(int));
+
+    /* squashing two loops into one.
+     * - Look in the array_of_blocklengths for any large values
+     * - convert MPI_Count items (if they are not too big) into int-sized items
+     * after this loop we will know if we can use MPI_type_hindexed or if we
+     * need a more complicated BigMPI-style struct-of-chunks.
+     *
+     * Why not use the struct-of-chunks in all cases?  HDF5 reported a bug,
+     * which I have not yet precicesly nailed down, but appears to have
+     * something to do with struct-of-chunks when the chunks are small */
+
+#ifdef USE_ORIGINAL_MPICH_3_2
+    for(i=0; i<count; i++) {
+        if (array_of_blocklengths[i] > INT_MAX) {
+            blocklens[i] = 1;
+            is_big=1;
+            type_create_contiguous_x(array_of_blocklengths[i], oldtype,  &(types[i]));
+        } else {
+            /* OK to cast: checked for "bigness" above */
+            blocklens[i] = (int)array_of_blocklengths[i];
+            MPI_Type_contiguous(blocklens[i], oldtype, &(types[i]));
+        }
+    }
+
+    if (is_big) {
+        ret = MPI_Type_create_struct(count, blocklens, array_of_displacements,
+                types, newtype);
+    } else {
+        ret = MPI_Type_create_hindexed(count, blocklens, array_of_displacements, oldtype, newtype);
+    }
+    for (i=0; i< count; i++)
+        MPI_Type_free(&(types[i]));
+#else
+    /* See https://github.com/pmodels/mpich/pull/3089 */
+    for (i = 0; i < count; i++) {
+        if (array_of_blocklengths[i] > INT_MAX) {
+            blocklens[i] = 1;
+            is_big = 1;
+            type_create_contiguous_x(array_of_blocklengths[i], oldtype, &(types[i]));
+        } else {
+            /* OK to cast: checked for "bigness" above */
+            blocklens[i] = (int) array_of_blocklengths[i];
+            types[i] = oldtype;
+        }
+    }
+
+    if (is_big) {
+        ret = MPI_Type_create_struct(count, blocklens, array_of_displacements, types, newtype);
+        for (i = 0; i < count; i++)
+            if (types[i] != oldtype)
+                MPI_Type_free(&(types[i]));
+    } else {
+        ret = MPI_Type_create_hindexed(count, blocklens, array_of_displacements, oldtype, newtype);
+    }
+#endif
+    free(types);
+    free(blocklens);
+
+    return ret;
+}
+#endif
