d06225b117f894903d5b7c2456d5ca2847bc52e1
jayeshkrishna <jayesh@mcs.anl.gov>
2019-02-28 15:22:57 -0600

    Adding a simple cmd line user option parser
    Adding a simple user option/argument parser for the ADIOS
    conversion tool.
    
    * The argparser allows user to define the cmd line options
    * A parser that parses argv is added
    * Provides help to the user
    * Allows standardizing the user option format, error checking
      of user arguments etc
    
    It is not used as of this commit.

---
+++ tools/adios2pio-nm/argparser.cxx
@@ -0,0 +1,75 @@
+#include "mpi.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <regex>
+#include <cassert>
+#include <stdexcept>
+#include "argparser.h"
+
+namespace adios2pio_utils{
+
+ArgParser &ArgParser::add_opt(const std::string &opt,
+                    const std::string &help_str)
+{
+    opts_map_[opt] = help_str;
+    return *this;
+}
+
+void ArgParser::parse(int argc, char *argv[])
+{
+    assert(argv);
+    prog_name_ = std::string(argv[0]);
+    const std::string OPT_RGX_STR("--([^=]+)=(.+)");
+    for (int i = 1; i < argc; i++)
+    {
+        std::regex opt_rgx(OPT_RGX_STR.c_str());
+        std::smatch match;
+        std::string argvi(argv[i]);
+        if (std::regex_search(argvi, match, opt_rgx) &&
+            (match.size() == 3))
+        {
+            if (opts_map_.count(match.str(1)) != 1)
+            {
+                std::cerr << "Error: Invalid option, "
+                          << match.str(1).c_str() << "\n";
+                throw std::runtime_error("Invalid option");
+            }
+            /* FIXME: No validation performed on the option value
+             * We could ask the user to specify option type when defining an 
+             * option
+             */
+            arg_map_[match.str(1)] = match.str(2);
+        }
+        else if ((argvi == "--help") || (argvi == "-h"))
+        {
+            print_usage(std::cout);
+            return;
+        }
+        else
+        {
+            std::cerr << "Error: Unable to parse option : " << argvi << "\n";
+            throw std::runtime_error("Invalid option");
+        }
+    }
+}
+
+bool ArgParser::has_arg(const std::string &opt) const
+{
+    return (arg_map_.count(opt) == 1);
+}
+
+void ArgParser::print_usage(std::ostream &ostr) const
+{
+    ostr << "Usage : " << prog_name_ << " [OPTIONAL ARGS] \n";
+    ostr << "Optional Arguments :\n";
+    for (std::map<std::string, std::string>::const_iterator
+          citer = opts_map_.cbegin();
+          citer != opts_map_.cend(); ++citer)
+    {
+        ostr << "--" << (*citer).first << ":\t" << (*citer).second << "\n";
+    }
+}
+
+} // namespace adios2pio_utils
