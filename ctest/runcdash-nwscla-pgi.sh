3d8956df31f40d84cf6f6372c682df6afc42f64c
Jim Edwards <jedwards@ucar.edu>
2020-10-29 08:19:51 -0600

    fix pgi version and openmpi test error

--- ctest/runcdash-nwscla-pgi.sh
+++ ctest/runcdash-nwscla-pgi.sh
@@ -12,7 +12,7 @@ module unload netcdf
 module swap intel pgi/20.4
 module swap mpt mpt/2.22
 module load git/2.22.0
-module load cmake/3.18.2
+module load cmake/3.16.4
 module load netcdf-mpi/4.7.3
 module load pnetcdf/1.12.1
 
@@ -20,7 +20,7 @@ export CC=mpicc
 export FC=mpif90
 export MPI_TYPE_DEPTH=24
 export PIO_DASHBOARD_ROOT=/glade/u/home/jedwards/sandboxes/dashboard
-export PIO_COMPILER_ID=PGI-`$CC --version | head -n 2 | tail -n 1 | cut -d' ' -f2`
+export PIO_COMPILER_ID=PGI-`$CC --version | head -n 2 | tail -n 1 | cut -d' ' -f4`
 
 if [ ! -d "$PIO_DASHBOARD_ROOT" ]; then
   mkdir "$PIO_DASHBOARD_ROOT"
