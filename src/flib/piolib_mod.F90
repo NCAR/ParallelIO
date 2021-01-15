c7b3010cc514c7b61033926678da9000f7004359
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-01-15 09:47:25 -0700

    documentation improvements

--- src/flib/piolib_mod.F90
+++ src/flib/piolib_mod.F90
@@ -968,7 +968,7 @@ contains
   !! pio operations (defined in PIO_types).
   !! @param base @em optional argument can be used to offset the first
   !! io task. Since this is an MPI task number, it is zero-based (the
-  !! first task is 0). The default base is task 1.
+  !! first task is 0). The default base is task 0.
   !! @param rearr_opts the rearranger options.
   !! @author Jim Edwards
   !<
