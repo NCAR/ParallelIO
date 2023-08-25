ab6c84b29db32d68d6b495f334fca3a708bc8004
James Edwards <jedwards@ucar.edu>
2023-08-25 17:38:52 -0600

    github tests again

--- src/clib/pio_nc4.c
+++ src/clib/pio_nc4.c
@@ -1395,7 +1395,7 @@ PIOc_def_var_filter(int ncid, int varid, unsigned int id, size_t nparams, unsign
 
     return PIO_NOERR;
 }
-#ifdef NC_HAS_PAR_FILTERS
+#ifdef PIO_HAS_PAR_FILTERS
 /**
  * Get the variable filter ids if any
  *
@@ -1796,7 +1796,7 @@ PIOc_inq_var_zstandard(int ncid, int varid, int* hasfilterp, int *levelp)
 // NC_HAS_ZSTD
 #endif
 #endif
-#ifdef NC_HAS_PAR_FILTERS
+#ifdef PIO_HAS_PAR_FILTERS
 /**
  *
  *
